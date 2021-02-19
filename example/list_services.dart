a32573e5c6e2a45cfe72dfd02546386262c7ebf0
Robert Ancell <robert.ancell@canonical.com>
2021-02-19 15:30:45 +1300

    Add initial GATT client support
    Fixes https://github.com/canonical/bluez.dart/issues/10

---
+++ example/list_services.dart
@@ -0,0 +1,47 @@
+import 'package:dbus/dbus.dart';
+import 'package:bluez/bluez.dart';
+
+void main() async {
+  var systemBus = DBusClient.system();
+  var client = BlueZClient(systemBus);
+  await client.connect();
+
+  if (client.adapters.isEmpty) {
+    print('No Bluetooth adapters found');
+    await systemBus.close();
+    return;
+  }
+
+  for (var device in client.devices) {
+    var services = device.gattServices;
+    if (services.isEmpty) {
+      continue;
+    }
+    print('Device ${device.alias}');
+    await device.connect();
+    for (var service in device.gattServices) {
+      print('  Service ${service.uuid.id}');
+      for (var characteristic in service.gattCharacteristics) {
+        String characteristicValue;
+        try {
+          characteristicValue = '${await characteristic.readValue()}';
+        } catch (e) {
+          characteristicValue = '<read failed: ${e}>';
+        }
+        print(
+            '    Characteristic ${characteristic.uuid.id} = ${characteristicValue}');
+        for (var descriptor in characteristic.gattDescriptors) {
+          String descriptorValue;
+          try {
+            descriptorValue = '${await descriptor.readValue()}';
+          } catch (e) {
+            descriptorValue = '<read failed: %{e}>';
+          }
+          print('      Descriptor ${descriptor.uuid.id} = ${descriptorValue}');
+        }
+      }
+    }
+  }
+
+  await systemBus.close();
+}
